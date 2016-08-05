0d8ee8226384100d90a70a78ae75bd0b10223faf
HealthyChoice <HealthyChoiceRBLX@Gmail.com>
2016-08-04 21:00:32 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,6 @@
 #Version: 1.0
 #Contact: @HealthyRBLX(twitter),HealthyChoicesRBLX@Gmail.com(email),HealthyChoice(github)
 
-#import requests
 from .account import account
 from .parser import parser,thread,post
 
-print('RobloxModule loaded')
