1eeaba32c558ae8d594b55264583abf4eaffd8b0
HealthyChoice <HealthyChoiceRBLX@Gmail.com>
2016-08-11 15:23:58 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='foruming',
+      version='1.0',
+      description='A module for roblox foruming',
+      url='https://github.com/HealthyChoice/foruming',
+      author='HealthyChoices',
+      install_requires=['requests'],
+      zip_safe=False)
+
