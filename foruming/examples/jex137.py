926e5fba3f696819fe6cb0c1f3d5fb9e01d86b07
HealthyChoice <HealthyChoiceRBLX@Gmail.com>
2016-08-12 11:05:18 -0400

    Update jex137.py

--- foruming/examples/jex137.py
+++ foruming/examples/jex137.py
@@ -1,4 +1,6 @@
-#
+#This is an example of usage of the foruming package
+#This script will automatically reply to any post that contains a keyword, 
+#similar to the jex137/sisa8447 bots that used to be active on the Off topic forum
 
 import foruming
 
